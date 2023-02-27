2df6257b03a773dea7c03d8231cb19a683279d23
Ümmühan Kurt <63405463+ummuhankurt@users.noreply.github.com>
2023-02-27 04:31:33 +0300

    first commit

---
+++ calculator.js
@@ -0,0 +1,36 @@
+//jshint eversion:6
+const express = require("express");
+const bodyParser = require("body-parser");
+const app = express();
+app.use(bodyParser.urlencoded({extended: true}));
+
+
+
+app.listen(3000,function(){
+    console.log("server started on 3000");
+});
+
+app.get("/",function(req,res){
+    res.sendFile(__dirname + "/index.html");
+});
+
+app.get("/bmicalculator",function(req,res){
+    res.sendFile(__dirname + "/bmiCalculator.html")
+});
+
+
+app.post("/bmicalculator",function(req,res){
+    var weight = Number(req.body.weight);
+    var height = Number(req.body.height);
+    var result = weight/(Math.pow(height,2));
+    res.send("Your BMI is " + result);
+});
+
+
+app.post("/",function(req,res){
+    var num1 = Number(req.body.num1);
+    var num2 = Number(req.body.num2);
+    var result = num1 + num2;   
+    res.send("The result of the calculate : " + result);
+});
+
